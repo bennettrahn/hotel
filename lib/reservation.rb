080e40827f249fda9301a751b41bf72ee55ddff3
Bennett Rahn <rahnibennett@gmail.com>
2017-09-05 16:38:03 -0700

    create basic Reservation class, realized need to create date_range class first

---
+++ lib/reservation.rb
@@ -0,0 +1,12 @@
+require 'date'
+
+module Hotel
+  class Reservation
+
+    def initialize(start_date, end_date)
+      @start_date = start_date
+      @end_date = end_date
+    end
+
+  end
+end
--- specs/reservation_spec.rb
+++ specs/reservation_spec.rb
@@ -2,5 +2,19 @@ require 'pry'
 require_relative 'spec_helper'
 
 describe 'Reservation class' do
+  before do
+    start_date = Date.new(2017,9,1)
+    end_date = Date.new(2017,9,3)
+    @reservation1 = Hotel::Reservation.new(start_date, end_date)
+  end
+  describe 'initialize' do
+    it 'can be instantiated' do
+      @reservation1.must_be_kind_of Hotel::Reservation
+    end
+
+    xit 'creates a DateRange' do
+
+    end
+  end
 
 end
