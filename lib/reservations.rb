262240e8bf4c5856aa16d3dcf4795183bf3f6463
Bennett Rahn <rahnibennett@gmail.com>
2017-09-06 09:48:42 -0700

    create initializing methods and tests for reservations class

--- lib/reservations.rb
+++ lib/reservations.rb
@@ -1,29 +1,45 @@
+require 'date'
 
-# Reservations
-# @all_rooms = room x 20
-# @all_reservations = [reservation, reservation]
-#
-# def make_reservation(no_of_rooms, start_date, end_date)
-#
-#   date_range = DateRange.new(start_date, end_date)
-#   # need to update reservation to accept date range as parameter rather than start, end
-#   #this throws error if dates are wrong right away - begin rescue?
-#   availability = check_availability(start_date, end_date)
-#   if availability.length < no_of_rooms
-#     ArgumentError "not enough rooms"
-#   elsif availability == []
-#     ArgumentError "no rooms avail"
-#   end
-#
-#   id = @all_reservations.length
-#   rooms = []
-#   no_of_rooms.times do |i|
-#     rooms << availability[i]
-#   end
-#
-#   @all_reservations << Reservation.new(id, rooms, date_range)
-#
-# end
+module Hotel
+  class Reservations
+    attr_reader :all_rooms, :all_reservations
+
+    def initialize
+      @all_rooms = []
+      create_rooms
+
+      @all_reservations = []
+
+    end
+
+    def create_rooms
+      20.times do |i|
+        num = i + 1
+        @all_rooms << Room.new(num)
+      end
+    end
+
+
+    # def make_reservation(no_of_rooms, start_date, end_date)
+    #
+    #   date_range = DateRange.new(start_date, end_date)
+    #   #this throws error if dates are wrong right away - begin rescue?
+    #   availability = check_availability(start_date, end_date)
+    #   if availability.length < no_of_rooms
+    #     ArgumentError "not enough rooms"
+    #   elsif availability == []
+    #     ArgumentError "no rooms avail"
+    #   end
+    #
+    #   id = @all_reservations.length
+    #   rooms = []
+    #   no_of_rooms.times do |i|
+    #     rooms << availability[i]
+    #   end
+    #
+    #   @all_reservations << Reservation.new(id, rooms, date_range)
+    #
+    # end
 #
 # def check_reserved(start_date, end_date)
 #   check_against = DateRange.new(start_date, end_date)
@@ -39,3 +55,5 @@
 # def check_availability
 #   return inverse of check_reserved
 # end
+  end
+end
--- specs/reservations_spec.rb
+++ specs/reservations_spec.rb
@@ -2,5 +2,34 @@ require 'pry'
 require_relative 'spec_helper'
 
 describe 'Reservations class' do
+  before do
+    @hotel_res = Hotel::Reservations.new
+    start_date = Date.new(2017,9,1)
+    end_date = Date.new(2017,9,3)
+    date_range = Hotel::DateRange.new(start_date, end_date)
+    room = Hotel::Room.new(1)
+    @reservation1 = Hotel::Reservation.new(1, [room], date_range)
+  end
+  describe 'initialize' do
+    it 'can be instantiated' do
+      @hotel_res.must_be_kind_of Hotel::Reservations
+    end
 
+    it 'creates all rooms' do
+      @hotel_res.all_rooms.must_be_kind_of Array
+      @hotel_res.all_rooms[0].must_be_kind_of Hotel::Room
+    end
+
+    it 'creates all_reservations' do
+      @hotel_res.all_reservations.must_be_kind_of Array
+    end
+  end
+
+  describe 'make_reservation' do
+    it 'creates a new reservation' do
+      @hotel_res.all_reservations[0].must_be_kind_of Hotel::Reservation
+    end
+
+
+  end
 end
