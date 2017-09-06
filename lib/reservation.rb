040fb3e6952970e18f8d2cabc900755650a42f96
Bennett Rahn <rahnibennett@gmail.com>
2017-09-05 18:48:17 -0700

    create reservation class and tests

--- lib/reservation.rb
+++ lib/reservation.rb
@@ -2,10 +2,18 @@ require 'date'
 
 module Hotel
   class Reservation
+    attr_reader :id, :date_range, :rooms, :total_cost
 
-    def initialize(start_date, end_date)
-      @start_date = start_date
-      @end_date = end_date
+    def initialize(id, start_date, end_date)
+      @id = id
+      @date_range = DateRange.new(start_date, end_date)
+      @rooms = []
+      @total_cost = 0
+    end
+
+    def add_room(room)
+      @rooms << room.number
+      @total_cost += room.cost
     end
 
   end
--- specs/reservation_spec.rb
+++ specs/reservation_spec.rb
@@ -5,15 +5,32 @@ describe 'Reservation class' do
   before do
     start_date = Date.new(2017,9,1)
     end_date = Date.new(2017,9,3)
-    @reservation1 = Hotel::Reservation.new(start_date, end_date)
+    @reservation1 = Hotel::Reservation.new(1, start_date, end_date)
   end
   describe 'initialize' do
     it 'can be instantiated' do
       @reservation1.must_be_kind_of Hotel::Reservation
     end
 
-    xit 'creates a DateRange' do
+    it 'creates a DateRange for the reservation' do
+      @reservation1.date_range.must_be_kind_of Hotel::DateRange
+    end
 
+    it 'creates an ID number' do
+      @reservation1.id.must_equal 1
+    end
+  end
+  describe 'add_room' do
+    it 'should add room number to reserved rooms' do
+      room1 = Hotel::Room.new(1)
+      @reservation1.add_room(room1)
+      @reservation1.rooms.length.must_equal 1
+    end
+
+    it 'should up the total_cost' do
+      room1 = Hotel::Room.new(1)
+      @reservation1.add_room(room1)
+      @reservation1.total_cost.must_equal 200
     end
   end
 
